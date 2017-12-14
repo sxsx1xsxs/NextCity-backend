0941606adaaf7ffc8ba363e5236a2bb18e916e1a
Yi Zhang <yz3206@columbia.edu>
2017-12-14 16:02:40 -0500

    initialize the project

---
+++ application.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from flask import render_template
+
+application = Flask(__name__)
+
+
+@application.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
+
