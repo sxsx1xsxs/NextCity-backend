000244653ae410eb83b6730506025c55f391e6e0
ReggieYang <1937730499@qq.com>
2017-12-16 17:17:31 -0500

    ~

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ from flask import render_template
 from users import *
 import json
 from database import *
-from collections import namedtuple
+from collections import namedtuple, Set
 from flask_cors import CORS
 
 application = Flask(__name__)
@@ -31,6 +31,7 @@ def login_user():
         store_user(user)
     return json.dumps(user)
 
+
 @application.route('/save_user', methods=['POST'])
 def save_user():
     user = json.loads(request.args['user'], object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
@@ -46,11 +47,14 @@ def fun_get_city():
     # return json.dumps(city, default=lambda o: o.__dict__)
     return json.dumps(city)
 
+
 '''
 sample url: http://127.0.0.1:5000/get_city_list_by_preference?email=Alonzo.Ball@gmail.com&list_size=100
 sample output: ["Oakland", "New York", "Honolulu", "Berkeley", "Queens", "San Francisco", "San Francisco", "Los Angeles", "New York" ...]
 there are duplicated in output
 '''
+
+
 @application.route('/get_city_list_by_preference')
 def fun_get_city_list():
     # user = json.loads(request.args['user'], object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
@@ -67,14 +71,26 @@ def fun_get_city_list():
     # city_list = search_city_by_preferences(preferences, search_size)
     # return json.dumps(city_list, default=lambda o: o.__dict__)
     cities = [other_user['address'] for other_user in other_users]
+    res = []
+    seen = set([])
+    cnt = 0
+    for city in cities:
+        if cnt == int(list_size):
+            break
+        if city not in seen:
+            seen.add(city)
+            res.append(city)
+            cnt += 1
 
-    return json.dumps(cities)
+    return json.dumps(res)
+
 
 @application.route('/get_all_jobs')
 def fun_get_all_jobs():
     # size = json.loads(request.args['size'], object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
     # jobs = get_all_jobs(size=size) if not size is None else get_all_jobs()
     return json.dumps(get_all_jobs())
+
 
 # @application.route('/search_job_by_skills')
 # def fun_get_job_list_by_skills():
