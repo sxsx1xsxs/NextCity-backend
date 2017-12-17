dee43e9b481c99e5cebe903a59a10a1a46aaeda2
ReggieYang <1937730499@qq.com>
2017-12-16 22:24:18 -0500

    ~

--- logic.py
+++ logic.py
@@ -7,13 +7,13 @@ def get_total_similarity(a, b):
     for i in range(len(b)):
         cnt = 0
         for v in a[compare1]:
-            if v in b[i][compare1]:
+            if b[i][compare1] is not None and v in b[i][compare1]:
                 cnt += 1
         b[i][score] = cnt / len(a[compare1]) / 2
 
         cnt = 0
         for j in range(len(a[compare2])):
-            if a[compare2][j] in b[i][compare2]:
+            if b[i][compare2] is not None and a[compare2][j] in b[i][compare2]:
                 cnt += abs(j - b[i][compare2].index(a[compare2][j]))
             else:
                 cnt += len(a[compare2])
