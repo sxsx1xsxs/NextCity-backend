f07140c1304cd2350635f619a3381cadead19773
ReggieYang <1937730499@qq.com>
2017-12-16 23:27:47 -0500

    ~

--- database.py
+++ database.py
@@ -66,22 +66,6 @@ def get_user_from_db(email):  # YKM
     except:
         print("Error: unable to fetch data")
         db.close()
-    # user = {}
-    # try:
-    #     cursor.execute(sql, email)
-    #     # fields_name = get_fields_name(cursor)
-    #     results = cursor.fetchall()
-    #     row = results[0]
-    #     for i in range(len(row)):
-    #         user[fields_name[i]] = row[i]
-    #     for field in ["skill", "other_pref", "main_pref"]:
-    #         tmp = list(user[field].strip().split(','))
-    #         user[field] = list(map(lambda x: x.strip(), tmp))
-    #     db.close()
-    #     return user
-    # except:
-    #     db.close()
-    #     print('Error: unable to fetch data')
 
 
 def store_user(user):  # QYY
@@ -94,20 +78,17 @@ def store_user(user):  # QYY
     # user is a dict mapping name, skill, main_pref, other_pref, email, password
     db = db_conn()
     cursor = db.cursor()
-    sql = "insert into user (name, skill, main_pref, other_pref, email, password, address) VALUES (%s, %s, %s, %s, %s, %s, %s)"
 
-    user['name'] = None if not 'name' in user else user['name']
-    user['skill'] = None if not 'skill' in user else user['skill']
-    user['main_pref'] = None if not 'main_pref' in user else user['main_pref']
-    user['other_pref'] = None if not 'other_pref' in user else user['other_pref']
-    user['email'] = None if not 'email' in user else user['email']
-    user['password'] = None if not 'password' in user else user['password']
-    user['address'] = None if not 'address' in user else user['address']
+    properties = ('name', 'skill', 'main_pref', 'other_pref', 'email', 'password', 'address')
+    sql = "insert into user (" + ",".join(properties) + ") VALUES (%s, %s, %s, %s, %s, %s, %s)"
+
+    for property in properties:
+        user[property] = None if not property in user else user[property]
 
     try:
         cursor.execute(sql, (
-        user["name"], user["skill"], user["main_pref"], user["other_pref"], user["email"], user["password"],
-        user["address"]))
+            user["name"], user["skill"], user["main_pref"], user["other_pref"], user["email"], user["password"],
+            user["address"]))
         db.commit()
         db.close()
         return True
@@ -183,11 +164,6 @@ def search_job_by_city(city_name, size=100):  # QYY
     :param size: int
     :return: [dict]
     """
-    # from job import Job
-    # job1 = Job("job1")
-    # job2 = Job("job2")
-    # jobs = [job1, job2]
-    # size = 100
     result = []
     db = db_conqyy()
     cursor = db.cursor()
@@ -297,11 +273,7 @@ def get_userfavorjobs(email):  # QYY
         for jobid in jobidslist:
             cursor.execute(sql2, jobid)
             rows = cursor.fetchall()
-            # print(rows)
-            # print(type(rows))
             result.append(rows[0])
-        # print(result)
-        # print(len(result))
         db.close()
         return result
     except:
@@ -317,7 +289,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    # print(get_user_from_db("Alonzo.Ball@gmail.com"))
-
-    # user = {"name": "qyy", "skill": "a,b,c", "main_pref": "e,g,h", "other_pref": "sf,d", "email": "KLJF", "password": "safe", "address": "203"}
-    # print(store_user(user))
